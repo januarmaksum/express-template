a712248d985413aefc9863e0ebcfbabb90a16c58
Januar Maksum <januarmaksum@gmail.com>
2025-04-21 14:19:18 +0700

    fix: error blank swagger 22

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,44 +1,17 @@
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
-import env from './configs/env.config';
+import fs from 'fs';
+import path from 'path';
+// import env from './configs/env.config';
 
-const isProduction = process.env.NODE_ENV;
-const publicApiServer =
-  isProduction === 'production' ? env.API_URL : `http://localhost:${env.PORT}`;
+// const isProduction = process.env.NODE_ENV;
+// const publicApiServer =
+//   isProduction === 'production' ? env.API_URL : `http://localhost:${env.PORT}`;
 
-const options: swaggerJSDoc.Options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Docs',
-      version: '1.0.0',
-      contact: {
-        name: 'Januar Maksum',
-        url: 'https://github.com/januarmaksum',
-        email: 'januarmaksum@gmail.com',
-      },
-      license: {
-        name: 'Licence - MIT',
-        url: 'https://opensource.org/licenses/MIT',
-      },
-      tags: [
-        {
-          name: 'Users',
-          description: 'User related endpoints',
-        },
-      ],
-    },
-    servers: [
-      {
-        url: publicApiServer,
-      },
-    ],
-  },
-  apis: ['./src/docs/*.yaml'],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+// Load Swagger JSON spec file
+const swaggerSpec = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'docs', 'swagger.json'), 'utf8')
+);
 
 export const setupSwagger = (app: Express) => {
   const uiOptions = {
