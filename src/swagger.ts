51402c9595723658427505951089ee887773ce30
Januar Maksum <januarmaksum@gmail.com>
2025-04-22 16:32:38 +0700

    fix: last swagger blank

--- src/swagger.ts
+++ src/swagger.ts
@@ -53,13 +53,13 @@ const swaggerSpec = swaggerJSDoc(options);
 
 export const setupSwagger = (app: Express) => {
   const uiOptions = {
-    // customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui.min.css',
-    // customJs: [
-    //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui-bundle.min.js',
-    //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui-standalone-preset.min.js',
-    // ],
-    // customfavIcon:
-    //   'https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/favicon-32x32.png',
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui-standalone-preset.min.js',
+    ],
+    customfavIcon:
+      'https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/favicon-32x32.png',
     customSiteTitle: 'API - Express Template',
   };
 
